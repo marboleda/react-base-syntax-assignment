640107ea30fd200b29d317dcb319a9299671cbe2
Marco Arboleda <arboleda.marco@gmail.com>
2020-08-12 12:58:19 -0400

    Add some styling using both inline styles and stylesheets

--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import './UserOutput.css'
 
 const userOutput = (props) => {
+    const style = {
+        width: "60vw",
+        padding: "15px",
+        margin: "15px auto",
+        border: "2px solid black",
+        backgroundColor: "gray",
+    };
+
     return (
-        <div className="userOutput">
+        <div className="userOutput" style={style}>
             <p>Username:</p>
             <p>{props.username}</p>
         </div>
